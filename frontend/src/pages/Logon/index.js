7bd68aae55f291a9e52d4a733e6e5a97f759eb74
paulomaateus <paulo.moreira@ccc.ufcg.edu.br>
2020-05-20 17:54:37 -0300

    front end finalizado

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,41 @@
 import React,{useState}  from 'react';
 import {FiLogIn} from 'react-icons/fi'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import api from '../../services/api';
 import  './styles.css';
 
 import logoImg from "../../assets/logo.svg"
 import heroesImg from '../../assets/heroes.png'
 
-export default function logon (){
+export default function Logon (){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault(); 
+        
+        try{
+            const response = await api.post('sessions', {id});
+            history.push('/profile')
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name);
+        }catch(err){
+            alert('erro');
+        }
     
+    }
+
     return(
     <div className="logon-container">
         <section className="form">
             <img src = {logoImg} alt = "Be the Hero"/>
-            <form>
+            <form onSubmit = {handleLogin}>
                 <h1>Faça seu logon</h1>
-                <input placeholder ="Sua ID"/>
+                <input 
+                    placeholder ="Sua ID"
+                    value = {id}
+                    onChange = { e => setId(e.target.value)}
+                    />
                 <button className = "button" type="submit">Entrar</button>
                 <Link className ="back-link" to = "/register">
                     <FiLogIn size = {16} color = "#E02041"/> 
--- frontend/src/pages/newIncident/index.js
+++ frontend/src/pages/newIncident/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import './styles.css'
 import logoImg from "../../assets/logo.svg"
 import {FiArrowLeft} from 'react-icons/fi'
+import api from '../../services/api'
 
 export default function NewIncident(){
+    const[title, setTitle] = useState('');
+    const[description, setDescription] = useState('');
+    const[value, setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
+    const history = useHistory();
+    
+    async function handleNewIncident(e){
+        e.preventDefault();
+        
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        try{
+           await api.post('incidents', data, {
+               headers: {
+                   Authorization : ongId
+               }
+           }) 
+           history.push('/profile')
+        } catch (err){
+            alert('Erro ao cadastrar caso. Tente novamente.')
+        }
+    }
+    
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -18,11 +46,22 @@ export default function NewIncident(){
                         Voltar para home
                     </Link>
                 </section>
-                <form>
-                    <input placeHolder="Título do caso" />
-                    <textarea placeHolder= "Descrição"/>
-                    <input placeHolder = "Valor em reais" />
-        
+                <form onSubmit = {handleNewIncident}>
+                    <input 
+                        placeHolder="Título do caso"
+                        value = {title}
+                        onChange = {e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeHolder= "Descrição"
+                        value = {description}
+                        onChange = {e => setDescription(e.target.value)}
+                    />
+                    <input 
+                        placeHolder = "Valor em reais"
+                        value = {value}
+                        onChange = {e => setValue(e.target.value)}
+                        />
                     <button className = "button" type="submit">Cadastrar</button>
                 </form>
             </div>
